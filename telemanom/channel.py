fb77351cabef6390ebcc1a45d36da7b96423b47c
lilanpei <lilanpei@gmail.com>
2024-04-19 01:23:03 +0200

    Translate TensorFlow Keras code into PyTorch

--- telemanom/channel.py
+++ telemanom/channel.py
@@ -1,6 +1,8 @@
 import numpy as np
 import os
+import torch
 import logging
+from torch.utils.data import TensorDataset, DataLoader
 
 logger = logging.getLogger('telemanom')
 
@@ -33,14 +35,19 @@ class Channel:
         self.config = config
         self.X_train = None
         self.y_train = None
+        self.X_valid = None
+        self.y_valid = None
         self.X_test = None
         self.y_test = None
         self.y_hat = None
         self.train = None
         self.test = None
+        self.train_loader = None
+        self.valid_loader = None
+        self.test_loader = None
 
     def shape_data(self, arr, train=True):
-        """Shape raw input streams for ingestion into LSTM. config.l_s specifies
+        """Shape raw input streams for ingestion into LSTM or ESN. config.l_s specifies
         the sequence length of prior timesteps fed into the model at
         each timestep t.
 
@@ -62,9 +69,21 @@ class Channel:
             np.random.shuffle(data)
             self.X_train = data[:, :-self.config.n_predictions, :]
             self.y_train = data[:, -self.config.n_predictions:, 0]  # telemetry value is at position 0
+
+            # Split the dataset into training and validation sets based on the validation_split ratio
+            valid_size = int(len(self.X_train) * self.config.validation_split)
+            train_dataset = TensorDataset(torch.Tensor(self.X_train[valid_size:]), torch.Tensor(self.y_train[valid_size:]))
+            valid_dataset = TensorDataset(torch.Tensor(self.X_train[:valid_size]), torch.Tensor(self.y_train[:valid_size]))
+            
+            # Create DataLoaders for training and validation sets
+            self.train_loader = DataLoader(train_dataset, batch_size=self.config.batch_size, shuffle=True)
+            self.valid_loader = DataLoader(valid_dataset, batch_size=self.config.batch_size, shuffle=False)
         else:
             self.X_test = data[:, :-self.config.n_predictions, :]
             self.y_test = data[:, -self.config.n_predictions:, 0]  # telemetry value is at position 0
+
+            test_dataset = TensorDataset(torch.Tensor(self.X_test), torch.Tensor(self.y_test))
+            self.test_loader = DataLoader(test_dataset, batch_size=self.config.batch_size, shuffle=False)
 
     def load_data(self):
         """
--- telemanom/modeling.py
+++ telemanom/modeling.py
@@ -1,49 +1,87 @@
-from keras.models import Sequential, load_model
-from keras.callbacks import History, EarlyStopping, Callback
-from keras.layers import LSTM, Dense, Activation, Dropout
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm
 import numpy as np
 import os
 import logging
+from telemanom.esn import reservoir as esn
 
-# suppress tensorflow CPU speedup warnings
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# suppress PyTorch CPU speedup warnings
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 logger = logging.getLogger('telemanom')
+
+
+# class LSTMModel(nn.Module):
+#     def __init__(self, input_size, hidden_size, output_size, num_layers, dropout):
+#         super(LSTMModel, self).__init__()
+#         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+#         self.dropout = nn.Dropout(dropout)
+#         self.fc = nn.Linear(hidden_size, output_size)
+
+#     def forward(self, x):
+#         out, _ = self.lstm(x)
+#         out = self.dropout(out)
+#         out = self.fc(out[:, -1, :])
+#         return out
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_sizes, output_size, num_layers, dropout):
+        super(LSTMModel, self).__init__()
+        self.hidden_sizes = hidden_sizes
+        self.num_layers = num_layers
+        self.num_directions = 1  # assuming unidirectional LSTM
+        self.lstms = nn.ModuleList()
+        self.lstms.append(nn.LSTM(input_size, hidden_sizes[0], num_layers=num_layers, dropout=dropout, batch_first=True))
+        for i in range(1, len(hidden_sizes)):
+            self.lstms.append(nn.LSTM(hidden_sizes[i-1], hidden_sizes[i], num_layers=num_layers, dropout=dropout, batch_first=True))
+        self.fc = nn.Linear(hidden_sizes[-1], output_size)
+
+    def forward(self, x):
+        hiddens = []
+        for lstm in self.lstms:
+            out, _ = lstm(x)
+            x = out
+            hiddens.append(out)
+        out = self.fc(out[:, -1, :])
+        return out#, hiddens
+
+class ESNModel(nn.Module):
+    def __init__(self, input_size, hidden_sizes, output_size):
+        super(ESNModel, self).__init__()
+        self.hidden_sizes = hidden_sizes
+        self.num_directions = 1  # assuming unidirectional LSTM
+        self.reserviors = nn.ModuleList()
+        self.reserviors.append(esn.Reservoir(input_size, hidden_sizes[0], "tanh"))
+        for i in range(1, len(hidden_sizes)):
+            self.reserviors.append(esn.Reservoir(hidden_sizes[i-1], hidden_sizes[i], "tanh"))
+        self.fc = nn.Linear(hidden_sizes[-1], output_size)
+
+    def forward(self, x):
+        hiddens = []
+        for reservior in self.reserviors:
+            out = reservior(x)
+            x = out
+            hiddens.append(out)
+        out = self.fc(out[:, -1, :])
+        return out#, hiddens
 
 
 class Model:
     def __init__(self, config, run_id, channel):
-        """
-        Loads/trains RNN and predicts future telemetry values for a channel.
-
-        Args:
-            config (obj): Config object containing parameters for processing
-                and model training
-            run_id (str): Datetime referencing set of predictions in use
-            channel (obj): Channel class object containing train/test data
-                for X,y for a single channel
-
-        Attributes:
-            config (obj): see Args
-            chan_id (str): channel id
-            run_id (str): see Args
-            y_hat (arr): predicted channel values
-            model (obj): trained RNN model for predicting channel values
-        """
-
         self.config = config
         self.chan_id = channel.id
         self.run_id = run_id
         self.y_hat = np.array([])
         self.model = None
+        self.esn = esn
+        self.input_size = channel.X_train.shape[2]
 
         if not self.config.train:
             try:
                 self.load()
             except FileNotFoundError:
-                path = os.path.join('data', self.config.use_id, 'models',
-                                    self.chan_id + '.h5')
-                logger.warning('Training new model, couldn\'t find existing '
-                               'model at {}'.format(path))
+                path = os.path.join('data', self.config.use_id, 'models', self.chan_id + '.pt')
+                logger.warning(f'Training new model, couldn\'t find existing model at {path}')
                 self.train_new(channel)
                 self.save()
         else:
@@ -54,10 +92,21 @@ class Model:
         """
         Load model for channel.
         """
-
         logger.info('Loading pre-trained model')
-        self.model = load_model(os.path.join('data', self.config.use_id,
-                                             'models', self.chan_id + '.h5'))
+
+        # if self.config.model_architecture == "ESN":
+        #     self.model = esn.Reservoir(input_size=self.input_size,
+        #                                hidden_size=self.config.hidden_size,
+        #                                activation="tanh")
+        # else:
+        #     self.model = LSTMModel(input_size=self.input_size,
+        #                            hidden_sizes=self.config.layers,
+        #                            output_size=self.config.n_predictions,
+        #                            num_layers=1,
+        #                            dropout=self.config.dropout)
+
+        self.model.load_state_dict(torch.load(os.path.join('data', self.config.use_id, 'models', self.chan_id + '.pt')))
+        self.model.eval()
 
     def train_new(self, channel):
         """
@@ -66,48 +115,72 @@ class Model:
         Args:
             channel (obj): Channel class object containing train/test data
                 for X,y for a single channel
+            esn (bool): flag indicating an echo state network model
         """
 
-        cbs = [History(), EarlyStopping(monitor='val_loss',
-                                        patience=self.config.patience,
-                                        min_delta=self.config.min_delta,
-                                        verbose=0)]
+        if self.config.model_architecture == "ESN":
+            self.model = ESNModel(input_size=self.input_size,
+                                       hidden_sizes=self.config.layers,
+                                       output_size=self.config.n_predictions)
+        else:
+            self.model = LSTMModel(input_size=self.input_size,
+                                   hidden_sizes=self.config.layers,
+                                   output_size=self.config.n_predictions,
+                                   num_layers=2,
+                                   dropout=self.config.dropout)
 
-        self.model = Sequential()
+        criterion = nn.MSELoss()
+        optimizer = optim.Adam(self.model.parameters(), lr=float(self.config.learning_rate))
 
-        self.model.add(LSTM(
-            self.config.layers[0],
-            input_shape=(None, channel.X_train.shape[2]),
-            return_sequences=True))
-        self.model.add(Dropout(self.config.dropout))
+        best_val_loss = float('inf')
+        epochs_since_improvement = 0
+        with tqdm(total=self.config.epochs) as pbar:
+            for epoch in range(self.config.epochs):
+                self.model.train()  # Set the model to training mode
+                train_loss = 0.0
+                for inputs, targets in channel.train_loader:
+                    optimizer.zero_grad()
+                    outputs = self.model(inputs)
+                    loss = criterion(outputs, targets)
+                    loss.backward()
+                    optimizer.step()
+                    train_loss += loss.item() * inputs.size(0)
 
-        self.model.add(LSTM(
-            self.config.layers[1],
-            return_sequences=False))
-        self.model.add(Dropout(self.config.dropout))
+                # Calculate average loss for the epoch
+                train_loss /= len(channel.train_loader.dataset)
 
-        self.model.add(Dense(
-            self.config.n_predictions))
-        self.model.add(Activation('linear'))
+                # Validate the model
+                self.model.eval()  # Set the model to evaluation mode
+                valid_loss = 0.0
+                with torch.no_grad():
+                    for inputs, targets in channel.valid_loader:
+                        outputs = self.model(inputs)
+                        loss = criterion(outputs, targets)
+                        valid_loss += loss.item() * inputs.size(0)
 
-        self.model.compile(loss=self.config.loss_metric,
-                           optimizer=self.config.optimizer)
+                # Calculate average loss for the validation set
+                valid_loss /= len(channel.valid_loader.dataset)
 
-        self.model.fit(channel.X_train,
-                       channel.y_train,
-                       batch_size=self.config.lstm_batch_size,
-                       epochs=self.config.epochs,
-                       validation_split=self.config.validation_split,
-                       callbacks=cbs,
-                       verbose=True)
+                pbar.set_description(f'Epoch [{epoch+1}/{self.config.epochs}], Train Loss: {train_loss:.4f}, Valid Loss: {valid_loss:.4f}')
+
+                if valid_loss < best_val_loss:
+                    best_val_loss = valid_loss
+                    epochs_since_improvement = 0
+                else:
+                    epochs_since_improvement += 1
+
+                if epochs_since_improvement >= self.config.patience:
+                    logger.info(f'Early stopping at epoch {epoch}')
+                    break
+
+                pbar.update(1)
 
     def save(self):
         """
         Save trained model.
         """
 
-        self.model.save(os.path.join('data', self.run_id, 'models',
-                                     '{}.h5'.format(self.chan_id)))
+        torch.save(self.model.state_dict(), os.path.join('data', self.run_id, 'models', f'{self.chan_id}.pt'))
 
     def aggregate_predictions(self, y_hat_batch, method='first'):
         """
@@ -151,11 +224,9 @@ class Model:
             channel (obj): Channel class object with y_hat values as attribute
         """
 
-        num_batches = int((channel.y_test.shape[0] - self.config.l_s)
-                          / self.config.batch_size)
+        num_batches = int((channel.y_test.shape[0] - self.config.l_s) / self.config.batch_size)
         if num_batches < 0:
-            raise ValueError('l_s ({}) too large for stream length {}.'
-                             .format(self.config.l_s, channel.y_test.shape[0]))
+            raise ValueError(f'l_s ({self.config.l_s}) too large for stream length {channel.y_test.shape[0]}.')
 
         # simulate data arriving in batches, predict each batch
         for i in range(0, num_batches + 1):
@@ -167,14 +238,13 @@ class Model:
                 idx = channel.y_test.shape[0]
 
             X_test_batch = channel.X_test[prior_idx:idx]
-            y_hat_batch = self.model.predict(X_test_batch)
+            y_hat_batch = self.model(torch.Tensor(X_test_batch)).detach().numpy()
             self.aggregate_predictions(y_hat_batch)
 
         self.y_hat = np.reshape(self.y_hat, (self.y_hat.size,))
 
         channel.y_hat = self.y_hat
 
-        np.save(os.path.join('data', self.run_id, 'y_hat', '{}.npy'
-                             .format(self.chan_id)), self.y_hat)
+        np.save(os.path.join('data', self.run_id, 'y_hat', f'{self.chan_id}.npy'), self.y_hat)
 
         return channel
